fefca6156edd99b6c0047de1d169265ed18ba96b
TypingTapestry <typingtapestry@gmail.com>
2022-04-26 01:25:00 -0500

    6kyu

---
+++ tribonacci sequence.js
@@ -0,0 +1,5 @@
+function tribonacci(signature, n) {
+    for (i = 3; i < n; i++) {
+      signature.push((signature[i - 1] + signature[i - 2] + signature[i - 3]));
+    } return signature.slice(0, n);
+}
