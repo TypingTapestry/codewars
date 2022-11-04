ba03715d6eab943b853f10c886159178a4d05963
TypingTapestry <typingtapestry@gmail.com>
2022-11-04 12:38:28 -0500

    6kyu

---
+++ are we alternate.js
@@ -0,0 +1,5 @@
+function isAlt(word) {
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 1; i < word.length; i++) if (vowels.indexOf(word[i]) < 0 && vowels.indexOf(word[i - 1]) < 0 || vowels.indexOf(word[i]) >= 0 && vowels.indexOf(word[i - 1]) >= 0) return false;
+    return true;
+}
